eb357618378ce567483a510a29cec0b04c4cf565
zzk0 <zzk9460@gmail.com>
2022-10-12 11:36:04 +0800

    optimize reduce sum:

--- cuda/reduce_sum/README.md
+++ cuda/reduce_sum/README.md
@@ -40,7 +40,11 @@ nvprof --metrics achieved_occupancy ./cuda_executable
 # 内核的内存读取效率
 nvprof --metrics gld_throughput ./cuda_executable
 # 全局加载效率：被请求的全局加载吞吐量占所需的全局加载吞吐量的比值
-nvprof --metrics gld_throughput ./cuda_executable
+nvprof --metrics gld_efficiency ./cuda_executable
+# 每个线程束上执行指令数量的平均值
+nvprof --metrics inst_per_warp ./cuda_executable
+# 设备内存读取吞吐量
+nvprof --metrics dram_read_throughput ./cuda_executable
 ```
 
 # 规约问题
@@ -50,3 +54,44 @@ nvprof --metrics gld_throughput ./cuda_executable
 - 朴素实现：每个元素依次相加
 - 相邻配对：元素和它们直接相邻的元素配对相加
 - 交错配对：根据跨度配对元素
+
+优化的结果：
+
+```
+CPU reduce sum time cost: 12655 us
+==1711099== NVPROF is profiling process 1711099, command: ./cuda/reduce_sum/reduce_sum
+GPU warmup time cost: 50 us
+time cost: 628 us       reduce neighbor 
+time cost: 352 us       reduce neighbor + less divergence 
+time cost: 316 us       reduce interleave 
+time cost: 267 us       reduce interleave + loop unrolling x 2 
+time cost: 170 us       reduce interleave + loop unrolling x 8 
+time cost: 170 us       reduce interleave + loop unrolling x 8 + warp unrolling 
+time cost: 169 us       reduce interleave + loop unrolling x 8 + warp unrolling + complete unroll 
+time cost: 166 us       reduce interleave + loop unrolling x 8 + warp unrolling + complete unroll + template 
+==1711099== Profiling application: ./cuda/reduce_sum/reduce_sum
+==1711099== Profiling result:
+            Type  Time(%)      Time     Calls       Avg       Min       Max  Name
+ GPU activities:   98.50%  127.90ms        10  12.790ms  1.8560us  14.657ms  [CUDA memcpy HtoD]
+                    0.45%  585.22us         2  292.61us  4.0960us  581.12us  void GpuReduceSum0<int>(int*, int*, unsigned int)
+                    0.24%  307.52us         1  307.52us  307.52us  307.52us  void GpuReduceSum1<int>(int*, int*, unsigned int)
+                    0.21%  271.74us         1  271.74us  271.74us  271.74us  void GpuReduceSum2<int>(int*, int*, unsigned int)
+                    0.17%  221.18us         1  221.18us  221.18us  221.18us  void GpuReduceSum3<int>(int*, int*, unsigned int)
+                    0.10%  128.67us         1  128.67us  128.67us  128.67us  void GpuReduceSum6<int>(int*, int*, unsigned int)
+                    0.10%  127.71us         1  127.71us  127.71us  127.71us  void GpuReduceSum5<int>(int*, int*, unsigned int)
+                    0.10%  127.39us         1  127.39us  127.39us  127.39us  void GpuReduceSum4<int>(int*, int*, unsigned int)
+                    0.10%  125.98us         1  125.98us  125.98us  125.98us  void GpuReduceSum7<int, unsigned int=512>(int*, int*, unsigned int)
+                    0.04%  49.632us         8  6.2040us  2.4640us  11.136us  [CUDA memcpy DtoH]
+      API calls:   61.89%  231.78ms        18  12.877ms  256.42us  220.96ms  cudaMalloc
+                   36.09%  135.17ms        18  7.5095ms  29.872us  15.216ms  cudaMemcpy
+                    0.76%  2.8330ms        18  157.39us  120.76us  186.48us  cudaFree
+                    0.52%  1.9302ms        10  193.02us  6.1160us  584.49us  cudaDeviceSynchronize
+                    0.46%  1.7237ms         2  861.83us  812.19us  911.48us  cuDeviceTotalMem
+                    0.17%  654.52us       194  3.3730us     159ns  173.86us  cuDeviceGetAttribute
+                    0.09%  331.95us         9  36.883us  33.211us  40.408us  cudaLaunchKernel
+                    0.02%  70.878us         2  35.439us  18.809us  52.069us  cuDeviceGetName
+                    0.00%  15.386us         2  7.6930us  2.7710us  12.615us  cuDeviceGetPCIBusId
+                    0.00%  2.8350us         4     708ns     189ns  2.1100us  cuDeviceGet
+                    0.00%  1.5550us         2     777ns     446ns  1.1090us  cuDeviceGetUuid
+                    0.00%  1.2330us         3     411ns     189ns     682ns  cuDeviceGetCount
+```
--- cuda/reduce_sum/main.cu
+++ cuda/reduce_sum/main.cu
@@ -15,8 +15,7 @@ __global__ void MathKernel1(float *c) {
   unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
   if (tid % 2 == 0) {
     a = 100.0f;
-  }
-  else {
+  } else {
     b = 200.0f;
   }
   c[tid] = a + b;
@@ -29,8 +28,7 @@ __global__ void MathKernel2(float *c) {
   unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
   if ((tid / warpSize) % 2 == 0) {
     a = 100.0f;
-  }
-  else {
+  } else {
     b = 200.0f;
   }
   c[tid] = a + b;
@@ -51,7 +49,7 @@ void TestCase0() {
     MathKernel1<<<1, size>>>(ptr.Get());
   }
   // PrintArray(ptr.GetHostPtr().get(), size);
-  
+
   {
     TimerRAII<std::chrono::microseconds> timer("MathKernel1: %d us\n");
     MathKernel1<<<1, size>>>(ptr.Get());
@@ -65,8 +63,7 @@ void TestCase0() {
   // PrintArray(ptr.GetHostPtr().get(), size);
 }
 
-template<typename T>
-bool CompareResult(const T a, const T b) {
+template <typename T> bool CompareResult(const T a, const T b) {
   float epsilon = 0.1;
   if (std::abs(float(a) - float(b)) > epsilon) {
     std::cout << a << " " << b << " ";
@@ -75,30 +72,283 @@ bool CompareResult(const T a, const T b) {
   return true;
 }
 
-template<typename T>
-void CpuReduceSum(T *idata, T *odata, unsigned int size) {
+template <typename T> void CpuReduceSum(T *idata, T *odata, unsigned int size) {
   odata[0] = 0.0f;
   for (unsigned int i = 0; i < size; ++i) {
     odata[0] += idata[i];
   }
 }
 
+typedef void (*ReduceFunctor)(int *, int *, unsigned int);
+
 /**
 reduce neighbor
 */
-template<typename T>
+template <typename T>
 __global__ void GpuReduceSum0(T *idata, T *odata, unsigned int size) {
-  unsigned int tid = threadIdx.x;
-  T *pidata = idata + (blockIdx.x * blockDim.x);
   if (blockIdx.x * blockDim.x + threadIdx.x >= size) {
     return;
   }
+  unsigned int tid = threadIdx.x;
+  T *pidata = idata + (blockIdx.x * blockDim.x);
   for (unsigned int stride = 1; stride < blockDim.x; stride *= 2) {
     if ((tid % (2 * stride)) == 0) {
       pidata[tid] += pidata[tid + stride];
     }
     __syncthreads();
   }
+  if (tid == 0) {
+    odata[blockIdx.x] = pidata[0];
+  }
+}
+
+/**
+reduce neighbor + less divergence
+*/
+template <typename T>
+__global__ void GpuReduceSum1(T *idata, T *odata, unsigned int size) {
+  if (blockIdx.x * blockDim.x + threadIdx.x >= size) {
+    return;
+  }
+  unsigned int tid = threadIdx.x;
+  T *pidata = idata + (blockIdx.x * blockDim.x);
+  for (unsigned int stride = 1; stride < blockDim.x; stride *= 2) {
+    unsigned int index = 2 * stride * tid;
+    if (index < blockDim.x) {
+      pidata[index] += pidata[index + stride];
+    }
+    __syncthreads();
+  }
+  if (tid == 0) {
+    odata[blockIdx.x] = pidata[0];
+  }
+}
+
+/**
+reduce interleave
+*/
+template <typename T>
+__global__ void GpuReduceSum2(T *idata, T *odata, unsigned int size) {
+  if (blockIdx.x * blockDim.x + threadIdx.x >= size) {
+    return;
+  }
+  unsigned int tid = threadIdx.x;
+  T *pidata = idata + blockIdx.x * blockDim.x;
+  for (unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
+    if (tid < stride) {
+      pidata[tid] += pidata[tid + stride];
+    }
+    __syncthreads();
+  }
+  if (tid == 0) {
+    odata[blockIdx.x] = pidata[0];
+  }
+}
+
+/**
+reduce interleave + loop unrolling x 2
+ps. 有一次运行 nvprof ./cuda/reduce_sum/reduce_sum 发生了数据不一致的情况
+*/
+template <typename T>
+__global__ void GpuReduceSum3(T *idata, T *odata, unsigned int size) {
+  unsigned int idx = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+  unsigned int tid = threadIdx.x;
+  T *pidata = idata + blockIdx.x * blockDim.x * 2;
+  if (idx + blockDim.x < size) {
+    pidata[idx] += pidata[idx + blockDim.x];
+  }
+  __syncthreads();
+
+  for (unsigned int stride = blockDim.x / 2; stride >= 1; stride >>= 1) {
+    if (tid < stride) {
+      pidata[tid] += pidata[tid + stride];
+    }
+    __syncthreads();
+  }
+  if (tid == 0) {
+    odata[blockIdx.x] = pidata[0];
+  }
+}
+
+/**
+reduce interleave + loop unrolling x 8
+*/
+template <typename T>
+__global__ void GpuReduceSum4(T *idata, T *odata, unsigned int size) {
+  unsigned int idx = blockIdx.x * blockDim.x * 8 + threadIdx.x;
+  unsigned int tid = threadIdx.x;
+  T *pidata = idata + blockIdx.x * blockDim.x * 8;
+  if (idx + blockDim.x * 7 < size) {
+    int a1 = pidata[idx + blockDim.x];
+    int a2 = pidata[idx + blockDim.x * 2];
+    int a3 = pidata[idx + blockDim.x * 3];
+    int a4 = pidata[idx + blockDim.x * 4];
+    int a5 = pidata[idx + blockDim.x * 5];
+    int a6 = pidata[idx + blockDim.x * 6];
+    int a7 = pidata[idx + blockDim.x * 7];
+    pidata[idx] += (a1 + a2 + a3 + a4 + a5 + a6 + a7);
+  }
+  __syncthreads();
+
+  for (unsigned int stride = blockDim.x / 2; stride >= 1; stride >>= 1) {
+    if (tid < stride) {
+      pidata[tid] += pidata[tid + stride];
+    }
+    __syncthreads();
+  }
+  if (tid == 0) {
+    odata[blockIdx.x] = pidata[0];
+  }
+}
+
+/**
+reduce interleave + loop unrolling x 8 + warp unrolling
+*/
+template <typename T>
+__global__ void GpuReduceSum5(T *idata, T *odata, unsigned int size) {
+  unsigned int idx = blockIdx.x * blockDim.x * 8 + threadIdx.x;
+  unsigned int tid = threadIdx.x;
+  T *pidata = idata + blockIdx.x * blockDim.x * 8;
+  if (idx + blockDim.x * 7 < size) {
+    int a1 = pidata[idx + blockDim.x];
+    int a2 = pidata[idx + blockDim.x * 2];
+    int a3 = pidata[idx + blockDim.x * 3];
+    int a4 = pidata[idx + blockDim.x * 4];
+    int a5 = pidata[idx + blockDim.x * 5];
+    int a6 = pidata[idx + blockDim.x * 6];
+    int a7 = pidata[idx + blockDim.x * 7];
+    pidata[idx] += (a1 + a2 + a3 + a4 + a5 + a6 + a7);
+  }
+  __syncthreads();
+
+  for (unsigned int stride = blockDim.x / 2; stride > 32; stride >>= 1) {
+    if (tid < stride) {
+      pidata[tid] += pidata[tid + stride];
+    }
+    __syncthreads();
+  }
+
+  if (tid < 32) {
+    volatile int *vmem = pidata;
+    vmem[tid] += vmem[tid + 32];
+    vmem[tid] += vmem[tid + 16];
+    vmem[tid] += vmem[tid + 8];
+    vmem[tid] += vmem[tid + 4];
+    vmem[tid] += vmem[tid + 2];
+    vmem[tid] += vmem[tid + 1];
+  }
+
+  if (tid == 0) {
+    odata[blockIdx.x] = pidata[0];
+  }
+}
+
+/**
+reduce interleave + loop unrolling x 8 + warp unrolling + complete unroll
+*/
+template <typename T>
+__global__ void GpuReduceSum6(T *idata, T *odata, unsigned int size) {
+  unsigned int idx = blockIdx.x * blockDim.x * 8 + threadIdx.x;
+  unsigned int tid = threadIdx.x;
+  T *pidata = idata + blockIdx.x * blockDim.x * 8;
+  if (idx + blockDim.x * 7 < size) {
+    int a1 = pidata[idx + blockDim.x];
+    int a2 = pidata[idx + blockDim.x * 2];
+    int a3 = pidata[idx + blockDim.x * 3];
+    int a4 = pidata[idx + blockDim.x * 4];
+    int a5 = pidata[idx + blockDim.x * 5];
+    int a6 = pidata[idx + blockDim.x * 6];
+    int a7 = pidata[idx + blockDim.x * 7];
+    pidata[idx] += (a1 + a2 + a3 + a4 + a5 + a6 + a7);
+  }
+  __syncthreads();
+
+  if (blockDim.x >= 1024 && tid < 512) {
+    pidata[tid] += pidata[tid + 512];
+  }
+  __syncthreads();
+
+  if (blockDim.x >= 512 && tid < 256) {
+    pidata[tid] += pidata[tid + 256];
+  }
+  __syncthreads();
+
+  if (blockDim.x >= 256 && tid < 128) {
+    pidata[tid] += pidata[tid + 128];
+  }
+  __syncthreads();
+
+  if (blockDim.x >= 128 && tid < 64) {
+    pidata[tid] += pidata[tid + 64];
+  }
+  __syncthreads();
+
+  if (tid < 32) {
+    volatile int *vmem = pidata;
+    vmem[tid] += vmem[tid + 32];
+    vmem[tid] += vmem[tid + 16];
+    vmem[tid] += vmem[tid + 8];
+    vmem[tid] += vmem[tid + 4];
+    vmem[tid] += vmem[tid + 2];
+    vmem[tid] += vmem[tid + 1];
+  }
+
+  if (tid == 0) {
+    odata[blockIdx.x] = pidata[0];
+  }
+}
+
+/**
+reduce interleave + loop unrolling x 8 + warp unrolling + complete unroll +
+template
+*/
+template <typename T, unsigned int t_block_size>
+__global__ void GpuReduceSum7(T *idata, T *odata, unsigned int size) {
+  unsigned int idx = blockIdx.x * blockDim.x * 8 + threadIdx.x;
+  unsigned int tid = threadIdx.x;
+  T *pidata = idata + blockIdx.x * blockDim.x * 8;
+  if (idx + blockDim.x * 7 < size) {
+    int a1 = pidata[idx + blockDim.x];
+    int a2 = pidata[idx + blockDim.x * 2];
+    int a3 = pidata[idx + blockDim.x * 3];
+    int a4 = pidata[idx + blockDim.x * 4];
+    int a5 = pidata[idx + blockDim.x * 5];
+    int a6 = pidata[idx + blockDim.x * 6];
+    int a7 = pidata[idx + blockDim.x * 7];
+    pidata[idx] += (a1 + a2 + a3 + a4 + a5 + a6 + a7);
+  }
+  __syncthreads();
+
+  if (t_block_size >= 1024 && tid < 512) {
+    pidata[tid] += pidata[tid + 512];
+  }
+  __syncthreads();
+
+  if (t_block_size >= 512 && tid < 256) {
+    pidata[tid] += pidata[tid + 256];
+  }
+  __syncthreads();
+
+  if (t_block_size >= 256 && tid < 128) {
+    pidata[tid] += pidata[tid + 128];
+  }
+  __syncthreads();
+
+  if (t_block_size >= 128 && tid < 64) {
+    pidata[tid] += pidata[tid + 64];
+  }
+  __syncthreads();
+
+  if (tid < 32) {
+    volatile int *vmem = pidata;
+    vmem[tid] += vmem[tid + 32];
+    vmem[tid] += vmem[tid + 16];
+    vmem[tid] += vmem[tid + 8];
+    vmem[tid] += vmem[tid + 4];
+    vmem[tid] += vmem[tid + 2];
+    vmem[tid] += vmem[tid + 1];
+  }
+
   if (tid == 0) {
     odata[blockIdx.x] = pidata[0];
   }
@@ -111,7 +361,8 @@ void TestCase1() {
   FillRandomNumber<int>(idata, element_cnt, -10, 10);
 
   {
-    TimerRAII<std::chrono::microseconds> timer("CPU reduce sum time cost: %d us\n");
+    TimerRAII<std::chrono::microseconds> timer(
+        "CPU reduce sum time cost: %d us\n");
     CpuReduceSum(idata.data(), &odata, element_cnt);
   }
 
@@ -119,36 +370,65 @@ void TestCase1() {
     DeviceUniquePointer<int> iptr(idata.data(), idata.size());
     DeviceUniquePointer<int> optr(&odata, 1);
     {
-      TimerRAII<std::chrono::microseconds> timer("GPU warmup time cost: %d us\n");
+      TimerRAII<std::chrono::microseconds> timer(
+          "GPU warmup time cost: %d us\n");
       GpuReduceSum0<<<1, 1>>>(iptr.Get(), optr.Get(), element_cnt);
       cudaDeviceSynchronize();
     }
   }
 
-  {
-    unsigned int block_size = 512;
-    unsigned int grid_size = (element_cnt + block_size - 1) / block_size;
+  const auto &ReduceSum = [&](const ReduceFunctor &functor,
+                              const std::string &functor_name,
+                              unsigned int block_size, unsigned int grid_size) {
     DeviceUniquePointer<int> iptr(idata.data(), idata.size());
     DeviceUniquePointer<int> optr(sizeof(int) * grid_size);
-    int gpu_sum = 0.0f;
+    int gpu_sum = 0;
     {
-      TimerRAII<std::chrono::microseconds> timer("GPU reduce sum time cost: %d us ");
-      GpuReduceSum0<<<{grid_size, 1}, {block_size, 1}>>>(iptr.Get(), optr.Get(), element_cnt);
+      TimerRAII<std::chrono::microseconds> timer("time cost: %d us \t" +
+                                                 functor_name + "\n");
+      functor<<<{grid_size, 1}, {block_size, 1}>>>(iptr.Get(), optr.Get(),
+                                                   element_cnt);
       cudaDeviceSynchronize();
     }
-    const auto& host_ptr = optr.GetHostPtr();  // keep this unique_ptr, otherwise gpu_odata will be released
+    const auto &host_ptr = optr.GetHostPtr(); // keep this unique_ptr, otherwise
+                                              // gpu_odata will be released
     int *gpu_odata = host_ptr.get();
     for (unsigned int i = 0; i < grid_size; ++i) {
       gpu_sum += gpu_odata[i];
     }
     bool ans = CompareResult(odata, gpu_sum);
-    if (ans) {
-      std::cout << "Equal" << std::endl;
+    if (!ans) {
+      std::cout << "\033[31mNot Equal\033[0m" << std::endl;
+      exit(1);
     }
-    else {
-      std::cout << "Not Equal" << std::endl;
-    }
-  }
+    // if (ans) {
+    //   std::cout << "\033[32mEqual\033[0m" << std::endl;
+    // } else {
+    //   std::cout << "\033[31mNot Equal\033[0m" << std::endl;
+    // }
+  };
+
+  constexpr unsigned int block_size = 512;
+  unsigned int grid_size = (element_cnt + block_size - 1) / block_size;
+  ReduceSum(GpuReduceSum0, "reduce neighbor ", block_size, grid_size);
+  ReduceSum(GpuReduceSum1, "reduce neighbor + less divergence ", block_size,
+            grid_size);
+  ReduceSum(GpuReduceSum2, "reduce interleave ", block_size, grid_size);
+  ReduceSum(GpuReduceSum3, "reduce interleave + loop unrolling x 2 ",
+            block_size, grid_size / 2);
+  ReduceSum(GpuReduceSum4, "reduce interleave + loop unrolling x 8 ",
+            block_size, grid_size / 8);
+  ReduceSum(GpuReduceSum5,
+            "reduce interleave + loop unrolling x 8 + warp unrolling ",
+            block_size, grid_size / 8);
+  ReduceSum(GpuReduceSum6,
+            "reduce interleave + loop unrolling x 8 + warp unrolling + "
+            "complete unroll ",
+            block_size, grid_size / 8);
+  ReduceSum(GpuReduceSum7<int, block_size>,
+            "reduce interleave + loop unrolling x 8 + warp unrolling + "
+            "complete unroll + template ",
+            block_size, grid_size / 8);
 }
 
 int main() {
